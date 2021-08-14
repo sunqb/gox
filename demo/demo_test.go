7f36a10796b078a3aa28f40f5d2bc4a23f0f4a93
sunqingbiao <1196239739@qq.com>
2021-08-14 14:52:16 +0800

    add vertx bridge

--- demo/demo_test.go
+++ demo/demo_test.go
@@ -2,10 +2,13 @@ package demo
 
 import (
 	"fmt"
+	"github.com/jponge/vertx-go-tcp-eventbus-bridge/eventbus"
 	"gox/etcd"
 	"gox/kv"
 	"gox/random"
+	"log"
 	"testing"
+	"time"
 )
 
 // TestAdd 单元测试
@@ -68,4 +71,71 @@ func TestGet(t *testing.T) {
 
 func TestRandom(t *testing.T) {
 	fmt.Println(random.GetRandomString(16))
+}
+
+// TestVertx 测试 vertx-go-tcp-eventbus-bridge
+func TestVertx(t *testing.T) {
+	stop := make(chan int, 1)
+
+	// 获取一个eventbus连接
+	eventBus, err := eventbus.NewEventBus("localhost:7000")
+	if err != nil {
+		// 打印错误日志并终止执行程序
+		log.Fatal("Connection to the Vert.x bridge failed: ", err)
+	}
+
+	// 获取一个dispatcher来持续接收messages
+	dispatcher := eventbus.NewDispatcher(eventBus)
+	dispatcher.Start()
+
+	// 在地址sample.clock.ticks 获取一个长度为8的队列(chan)
+	ticks, id, err := dispatcher.Register("sample.clock.ticks", 8)
+	if err != nil {
+		log.Fatal("Registration on sample.clock.ticks failed: ", err)
+	}
+	log.Println("sample.clock.ticks channel registered with ID=", id)
+
+	// 消费消息-方式1：开一个新协程，在里面运行匿名的函数。
+	go func() {
+		for {
+			// 从ticks里面读取数据，不用变量存储，就是玩。
+			reply := <-ticks
+			log.Println("[tick]:", reply)
+		}
+	}()
+
+	// 消费消息-方式2
+	//go func() {
+	//	for reply := range ticks {
+	//		log.Println("Echo, got back: ", reply)
+	//	}
+	//}()
+
+	// 生产消息-方式1
+	go func() {
+		for {
+			// 这一行每3s生产一次消息
+			<-time.After(time.Second * 3)
+			eventBus.SendWithReplyAddress("sample.echo", "sample.echo.reply", nil, map[string]string{
+				"source": "Go Client",
+				"what":   "A tick!",
+			})
+		}
+	}()
+
+	// 生产消息-方式2
+	//go func() {
+	//	for {
+	//		select {
+	//		case <-time.After(time.Second * 3):
+	//			eventBus.SendWithReplyAddress("sample.echo", "sample.echo.reply", nil, map[string]string{
+	//				"source": "Go Client",
+	//				"what":   "A tick!",
+	//			})
+	//		}
+	//	}
+	//}()
+
+	// 从停止管道里面读取消息判断程序是否做停止操作，需要程序员自己做
+	<-stop
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/jponge/vertx-go-tcp-eventbus-bridge v0.0.0-20180315155916-c9060137d6c9
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	go.etcd.io/etcd/client/v3 v3.5.0
 	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40
